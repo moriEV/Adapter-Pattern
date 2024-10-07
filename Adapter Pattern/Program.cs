9a180342430682644969d7ea479d21cc11530eea
KarpenkoEV <KarpenkoEV@309D-8>
2024-10-07 10:28:01 +0700

    Добавьте файлы проекта.

---
+++ Adapter Pattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using Adapter_Pattern;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ComputerGame computerGame = new ComputerGame(name:"Hamster Combat",pegiAgeRating:PegiAgeRating.P7,budgetInMillionsOfDollars:1,minimumGpuMemoryInMegabytes:10,diskSpaceNeededInGB:1,ramNeededInGb:1,coresNeeded:1,coreSpeedInGhz:2.4);
+        ComputerGameAdapter adapter = new ComputerGameAdapter(computerGame);
+        Console.WriteLine(adapter.getTitle());
+        Console.WriteLine(adapter.getPegiAllowedAge());
+        Console.WriteLine(adapter.getRequirements().ToString());
+        if (adapter.isTripleAGame())
+        {
+            Console.WriteLine("TripleA Game");
+        }
+        else
+        {
+            Console.WriteLine("Not tripleA game");
+        }
+    }
+}
